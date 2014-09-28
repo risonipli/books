65a2a1fccf8de2975a79b5dc6a5e2a43ba52ae52
Artem Prokhorov <risonipli@gmail.com>
2014-09-28 14:51:10 +0200

    complete exercise 01

--- scala_for_the_impatient/src/main/scala/ScalaBasic.scala
+++ scala_for_the_impatient/src/main/scala/ScalaBasic.scala
@@ -7,4 +7,19 @@ object ScalaBasic {
   возвести результат в квадрат
    */
   def task2 = pow(sqrt(3), 2)
+
+  /*
+   * Умножить строку 'crazy' на 3
+   */
+  def task4 = "crazy" * 3
+
+  /*
+   * Используя число типа BigInt
+   * вычислите 2 в степени 1024
+   */
+  def task6 = BigInt(2).pow(1024)
+
+  def getFirstLetter(s: String) = s.headOption.getOrElse("")
+
+  def getLastLetter(s: String) = s.lastOption.getOrElse("")
 }
--- scala_for_the_impatient/src/test/scala/ScalaBasicTest.scala
+++ scala_for_the_impatient/src/test/scala/ScalaBasicTest.scala
@@ -2,6 +2,24 @@ import org.scalatest.FunSuite
 
 class ScalaBasicTest extends FunSuite {
   test("Check pow(sqrt(3), 2)") {
-    assert(ScalaBasic.task2 > 0)
+    assert(ScalaBasic.task2 ===2.9999999999999996)
+  }
+
+  test("Check 'crazy' * 3 is equal 'crazycrazycrazy'") {
+    assert(ScalaBasic.task4 === "crazycrazycrazy")
+  }
+
+  test("Check 2^1024") {
+    assert(ScalaBasic.task6 === BigInt("179769313486231590772930519078902473361797697894230657273430081157732675805500963132708477322407536021120113879871393357658789768814416622492847430639474124377767893424865485276302219601246094119453082952085005768838150682342462881473913110540827237163350510684586298239947245938479716304835356329624224137216"))
+  }
+
+  test("First letter of 'Jagermeister' is 'J'") {
+    assert(ScalaBasic.getFirstLetter("Jagermeister") === 'J')
+    assert(ScalaBasic.getFirstLetter("") === "")
+  }
+
+  test("Last letter of 'Jagermeister' is 'r'") {
+    assert(ScalaBasic.getLastLetter("Jagermeister") === 'r')
+    assert(ScalaBasic.getLastLetter("") === "")
   }
 }
